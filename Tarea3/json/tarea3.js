9dcc2a9d6ac94e8823764c6c8cf5eb692cb552cf
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-06-01 13:29:14 +0200

    Ejercicio 3

---
+++ Tarea3/json/tarea3.js
@@ -0,0 +1,11 @@
+function parrafo() {
+    alert('Se presionó un párrafo del documento');
+}
+
+function parrafoTablaUno() {
+    alert('Se presionó un párrafo de la primera tabla');
+}
+
+function parrafoTablaDos() {
+    alert('Se presionó un párrafo de la segunda tabla');
+}
